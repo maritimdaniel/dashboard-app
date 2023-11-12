677dc506c7a2cdd78fe490c62d1ea897d70f9e67
Maritim Daniel <maritimdaniel07@gmai.com>
2023-11-12 23:14:24 +0300

    add test

--- src/components/dashboard/__tests__/Dashboard.test.js
+++ src/components/dashboard/__tests__/Dashboard.test.js
@@ -1,1 +1,25 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Dashboard from "./Dashboard";
+import mockDashboards from "./mockDashboards.json";
 
+jest.mock("./mockDashboards.json", () => mockDashboards);
+
+test("should render the DashboardCard components", () => {
+  render(<Dashboard />);
+  const dashboardCards = screen.getAllByRole("article");
+  expect(dashboardCards).toHaveLength(5);
+});
+
+test("should display the error state when there is an error", () => {
+  const originalFetch = global.fetch;
+  global.fetch = jest.fn(() =>
+    Promise.reject(new Error("Failed to fetch dashboards"))
+  );
+
+  render(<Dashboard />);
+  const error = screen.getByRole("alert");
+  expect(error).toBeInTheDocument();
+
+  global.fetch = originalFetch;
+});
---
+++ src/components/dashboard/__tests__/Filter.test.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import Filter from "./Filter";
+
+test("should display the 'Filter items' label", () => {
+  render(<Filter />);
+  const filterLabel = screen.getByLabelText("Filter items");
+  expect(filterLabel).toBeInTheDocument();
+});
+
+test("should render the filter select options", () => {
+  render(<Filter />);
+  const filterOptions = screen.getAllByRole("option");
+  expect(filterOptions).toHaveLength(4);
+});
+
+test("should display the default selected filter option", () => {
+  render(<Filter />);
+  const selectedOption = screen.getByRole("option", { selected: true });
+  expect(selectedOption.textContent).toBe("All types");
+});
+
+test("should update the selected filter option on change", () => {
+  render(<Filter />);
+  const visualizationOption = screen.getByRole("option", {
+    name: "Visualization",
+  });
+  fireEvent.click(visualizationOption);
+
+  const selectedOption = screen.getByRole("option", { selected: true });
+  expect(selectedOption.textContent).toBe("Visualization");
+});
