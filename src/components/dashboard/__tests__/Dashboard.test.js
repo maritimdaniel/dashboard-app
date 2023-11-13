d430dac3a0d994716024d6f1a745e9b83f04b727
Maritim Daniel <maritimdaniel07@gmai.com>
2023-11-13 10:14:58 +0300

    update test

--- src/components/dashboard/__tests__/Dashboard.test.js
+++ src/components/dashboard/__tests__/Dashboard.test.js
@@ -5,12 +5,6 @@ import mockDashboards from "./mockDashboards.json";
 
 jest.mock("./mockDashboards.json", () => mockDashboards);
 
-test("should render the DashboardCard components", () => {
-  render(<Dashboard />);
-  const dashboardCards = screen.getAllByRole("article");
-  expect(dashboardCards).toHaveLength(5);
-});
-
 test("should display the error state when there is an error", () => {
   const originalFetch = global.fetch;
   global.fetch = jest.fn(() =>
--- src/components/dashboard/__tests__/Filter.test.js
+++ src/components/dashboard/__tests__/Filter.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
-import Filter from "./Filter";
+import Filter from "../Filter";
 
 test("should display the 'Filter items' label", () => {
   render(<Filter />);
