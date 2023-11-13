9952bf334522db760e35c24ef48f9879bff9d98d
Maritim Daniel <maritimdaniel07@gmai.com>
2023-11-13 10:16:49 +0300

    update test

--- src/components/dashboard/__tests__/Dashboard.test.js
+++ src/components/dashboard/__tests__/Dashboard.test.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import Dashboard from "./Dashboard";
-import mockDashboards from "./mockDashboards.json";
-
-jest.mock("./mockDashboards.json", () => mockDashboards);
 
 test("should display the error state when there is an error", () => {
   const originalFetch = global.fetch;
